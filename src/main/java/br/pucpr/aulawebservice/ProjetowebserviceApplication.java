c44fee9fdc1f4bd9433a7d4ccfe9e3c13d08c1cb
HelenLauren <helenbonato02@gmail.com>
2025-09-09 19:04:48 -0300

    Innit Project base code

---
+++ src/main/java/br/pucpr/aulawebservice/ProjetowebserviceApplication.java
@@ -0,0 +1,13 @@
+package br.pucpr.aulawebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetowebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetowebserviceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/pucpr/aulawebservice/controller/UsuarioController.java
@@ -0,0 +1,4 @@
+package br.pucpr.aulawebservice.controller;
+
+public class UsuarioController {
+}
