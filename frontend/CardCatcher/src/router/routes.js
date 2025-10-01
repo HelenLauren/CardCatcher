e8fd5b709cef85eacee51397d99af6c8ab8eb527
Helen Lauren Bonato <167373993+HelenLauren@users.noreply.github.com>
2025-10-01 11:42:42 +0000

    Add base code for login page

---
+++ frontend/CardCatcher/src/router/routes.js
@@ -0,0 +1,21 @@
+
+const routes = [
+  { path: '/', component: () => import('pages/login.vue') },
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '/home', component: () => import('pages/home.vue') },
+    ]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
