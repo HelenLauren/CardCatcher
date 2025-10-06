828d8099d3efd0df749a4dbb2ba971d156c7099d
Helen Lauren Bonato <167373993+HelenLauren@users.noreply.github.com>
2025-10-06 12:08:38 +0000

    Added Repositories

---
+++ backend/src/main/java/br/pucpr/aulawebservice/repository/UserCollectionCardRepository.java
@@ -0,0 +1,6 @@
+package br.pucpr.aulawebservice.repository;
+
+import br.pucpr.aulawebservice.model.UserCollectionCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserCollectionCardRepository extends JpaRepository<UserCollectionCard, Long> {}
