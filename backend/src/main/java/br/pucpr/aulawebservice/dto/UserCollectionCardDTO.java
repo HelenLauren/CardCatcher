a852f3119d2f58e474dac2c913a74d0511421066
Helen Lauren Bonato <167373993+HelenLauren@users.noreply.github.com>
2025-10-06 13:21:34 +0000

    Changed from PostgreSQL to MySQL

--- backend/src/main/java/br/pucpr/aulawebservice/dto/UserCollectionCardDTO.java
+++ backend/src/main/java/br/pucpr/aulawebservice/dto/UserCollectionCardDTO.java
@@ -3,7 +3,13 @@ package br.pucpr.aulawebservice.dto;
 import br.pucpr.aulawebservice.model.UserCollectionCard;
 
 public record UserCollectionCardDTO(Long id, CardDTO card, String tradeStatus) {
+
     public static UserCollectionCardDTO fromEntity(UserCollectionCard c) {
-        return new UserCollectionCardDTO(c.getId(), CardDTO.fromEntity(c.getCard()), c.getTradeStatus().name());
+
+        return new UserCollectionCardDTO(
+            c.getId(), 
+            CardDTO.fromEntity(c.getCard()), 
+            c.getTradeStatus().name()
+        );
     }
 }
